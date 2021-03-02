2f3daa047ac3a56e95f35fc0379e18da2613c61e
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-03-02 17:13:40 -0500

    Fixed margin on title

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+// function getImageOfTheDay() {
+// 	fetch(
+// 		'https://api.nasa.gov/planetary/apod?api_key=I0V3Txv6ccRe18I7YmtzrolUSB3BB7LtAEwGlr42'
+// 	)
+// 		.then((response) => {
+// 			return response.json();
+// 		})
+// 		.then((data) => {
+// 			const imageElement = $('#mars-img-container');
+// 			imageElement.src = data.url;
+// 		})
+// 		.catch((error) => {
+// 			console.log('ERROR: ', error);
+// 		});
+// }
+// getImageOfTheDay();
+
 function getImageOfTheDay() {
 	fetch(
 		'https://api.nasa.gov/planetary/apod?api_key=I0V3Txv6ccRe18I7YmtzrolUSB3BB7LtAEwGlr42'
@@ -7,7 +24,7 @@ function getImageOfTheDay() {
 		})
 		.then((data) => {
 			const imageElement = $('#mars-img-container');
-			imageElement.src = data.url;
+			imageElement.style.backgroundImage = 'url(data.url)';
 		})
 		.catch((error) => {
 			console.log('ERROR: ', error);
