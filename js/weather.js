5b2d31ac05d49bac16ed225d1dab2ae4220aa2bf
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-03-03 13:22:42 -0500

    Did some stuff

--- js/weather.js
+++ js/weather.js
@@ -3,9 +3,20 @@
 const API_KEY = 'I0V3Txv6ccRe18I7YmtzrolUSB3BB7LtAEwGlr42'
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
 
+const currentSolElement = document.querySelector('[data-current-sol]')
+
+let selectedSolIndex
+
 getWeather().then(sols => {
-	console.log(sols)
+	selectedSolIndex = sols.length - 1
+	// console.log(sols)
+	displaySelectSol(sols)
 })
+
+function displaySelectSol(sols) {
+	const selectedSol = sols[selectedSolsIndex]
+	currentSolElement.innerText = selectedSol.sol
+}
 
 function getWeather() {
 	return fetch(API_URL)
