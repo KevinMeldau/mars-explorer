8d185d66cc86fb7400ca27ec5fcc74ad66db8bc0
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-03-03 13:09:26 -0500

    Pulling in data from weather API

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,29 @@
 // ------------------------------------- Weather on MARS ------------------------------------- //
-const API_KEY = 'I0V3Txv6ccRe18I7YmtzrolUSB3BB7LtAEwGlr42';
-const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
-function getWeather() {
-	fetch(API_URL)
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data);
-		});
-}
 
-getWeather();
+const API_KEY = 'I0V3Txv6ccRe18I7YmtzrolUSB3BB7LtAEwGlr42'
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
+
+getWeather().then(sols => {
+	console.log(sols)
+})
+
+function getWeather() {
+	return fetch(API_URL)
+		.then(res => res.json())
+		.then(resData => {
+			const { sol_keys, validity_checks, ...solData } = resData
+			return Object.entries(solData).map(([sol, data]) => {
+				return {
+					sol: sol,
+					maxTemp: data.PRE.mx,
+					minTemp: data.PRE.mn,
+					// windSpeed: data.HWS.av,
+					// windDirectionDegrees: data.WD.most_common.compass_degrees,
+					// windDirectionCardinal: data.WD.most_common.compass_point,
+					date: new Date(data.First_UTC),
+				}
+			})
+		})
+}
 
 // ------------------------------------- End Weather on MARS ------------------------------------- //
