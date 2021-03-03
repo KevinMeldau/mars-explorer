fb73c8354ae7b6ce73b2df6a8a2ec86090e3e59d
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-03-03 09:45:03 -0500

    Added in comments to think through the functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+// ------------------------------------- MARS Image of the day ------------------------------------- //
+
 function getImageOfTheDay() {
+	// This reaches out to the API and pulls in the data
 	fetch(
 		'https://api.nasa.gov/planetary/apod?api_key=I0V3Txv6ccRe18I7YmtzrolUSB3BB7LtAEwGlr42'
 	)
+		// We then take what we've received back from the API and turn it into json
 		.then((response) => {
 			return response.json();
 		})
+		// We then take the stuff in json format and we inject it into the image background url
 		.then((data) => {
 			$('#mars-img-container').css(
 				'background-image',
@@ -12,13 +17,18 @@ function getImageOfTheDay() {
 			);
 			console.log('Mars image');
 		})
+		// This is just a catch so that if anything goes wrong, there is something to tell us in the console
 		.catch((error) => {
 			console.log('ERROR: ', error);
 		});
 }
+// This just calls the function
 getImageOfTheDay();
 
+// ------------------------------------- End MARS Image of the day ------------------------------------- //
+
 // ------------------------------------- MODAL ------------------------------------- //
+
 // Modal functionality
 const modal = document.getElementById('marsModal');
 
