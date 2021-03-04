5fcd5b2d381dda4536fbf4ec70ad48241319fe02
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-03-03 20:15:44 -0500

    Fix weather API functionality

--- js/weather.js
+++ js/weather.js
@@ -4,18 +4,22 @@ const API_KEY = 'I0V3Txv6ccRe18I7YmtzrolUSB3BB7LtAEwGlr42'
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
 
 const currentSolElement = document.querySelector('[data-current-sol]')
+const currentSolTempHigh = document.querySelector('[data-current-temp-high]')
+const currentSolTempLow = document.querySelector('[data-current-temp-low]')
 
-let selectedSolIndex
+var selectedSolIndex = 0
 
 getWeather().then(sols => {
 	selectedSolIndex = sols.length - 1
-	// console.log(sols)
+	//console.log(sols)
 	displaySelectSol(sols)
 })
 
 function displaySelectSol(sols) {
-	const selectedSol = sols[selectedSolsIndex]
+	const selectedSol = sols[selectedSolIndex]
 	currentSolElement.innerText = selectedSol.sol
+	currentSolTempHigh.innerText = selectedSol.maxTemp.toFixed(2)
+	currentSolTempLow.innerText = selectedSol.minTemp.toFixed(2)
 }
 
 function getWeather() {
@@ -28,9 +32,9 @@ function getWeather() {
 					sol: sol,
 					maxTemp: data.PRE.mx,
 					minTemp: data.PRE.mn,
-					// windSpeed: data.HWS.av,
-					// windDirectionDegrees: data.WD.most_common.compass_degrees,
-					// windDirectionCardinal: data.WD.most_common.compass_point,
+					//windSpeed: data.HWS.av,
+					//windDirectionDegrees: data.WD.most_common.compass_degrees,
+					//windDirectionCardinal: data.WD.most_common.compass_point,
 					date: new Date(data.First_UTC),
 				}
 			})
